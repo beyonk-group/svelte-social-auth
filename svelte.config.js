60faaeae1ccac9cdcd2787299f1397d72cf0d0e3
Antony Jones <antony@beyonk.com>
2022-04-26 13:46:42 +0100

    lint

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,14 @@ import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		package: {
-			exports: filepath => {
+  kit: {
+    adapter: adapter(),
+    package: {
+      exports: filepath => {
         return filepath.endsWith('.js') || filepath.endsWith('.json')
       }
-		}
-	}
-};
+    }
+  }
+}
 
 export default config
